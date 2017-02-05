736705a1ead25a86549036010c63db14dd45de74
oojahooo <oojahooo@gmail.com>
2017-02-05 21:42:22 +0900

    mentoring

---
+++ latexmentoring/outline/auto/outline.el
@@ -0,0 +1,49 @@
+(TeX-add-style-hook
+ "outline"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "kotex")
+   (LaTeX-add-labels
+    "sec:orgb007ac3"
+    "sec:org2de5bbd"
+    "sec:org8195316"
+    "sec:org9f622fe"
+    "sec:org38cb18f"
+    "sec:orgfe24346"
+    "sec:org48d3943"
+    "sec:org7b4c559"
+    "sec:org671eb64"
+    "sec:org21b3205"
+    "sec:org6c6e510"
+    "sec:org4d53b5b"))
+ :latex)
+
